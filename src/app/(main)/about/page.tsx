9bdc00c15e693f84b08da3f2cf05bd448b11c85a
Zoila Consultora Creativa <consultora.creativa@zoila.cl>
2025-10-12 09:50:40 +0000

    volvimos a lo mismo en la versión desktop al menos, sigo viendo lo mismo

--- src/app/(main)/about/page.tsx
+++ src/app/(main)/about/page.tsx
@@ -61,7 +61,7 @@ export default function AboutPage() {
       </section>
 
       <section className="py-24 md:py-32">
-        <div className="container flex flex-col md:flex-row gap-16 md:gap-24">
+        <div className="container flex flex-col md:flex-row gap-16 md:gap-24 items-start">
           <div className="w-full md:w-1/2">
             <div className="relative w-full h-full">
               <Image
